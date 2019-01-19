84b73eff0e357395bf86614986aa3807743a0ea6
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-19 01:41:26 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
-
+  def index
+    @games = Game.all
+  end
+  
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.create
+    render json: @game, status: 201
+  end
+  
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, status: 200
+  end
+  
+  def update
+    @game.update
+    render json: @game, status: 202
+  end  
+  
+  private
+  
+  def game_params
+    params.require(:game).permit(:state)
+  end
 end
